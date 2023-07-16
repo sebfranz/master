213b04ed9c871e331c25a38522d1dbf7d1c74ed5
filipBirve <birve.f+github@gmail.com>
2023-07-16 22:10:07 +0200

    add QoL function for path to current file

---
+++ functions/QoL/getCurrentFileLocation.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+getCurrentFileLocation <-  function()
+{
+  this_file <- commandArgs() %>%
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+  if (length(this_file)==0)
+  {
+    this_file <- rstudioapi::getSourceEditorContext()$path
+  }
+  return(dirname(this_file))
+}
