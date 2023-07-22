b63295542aec64240244d820a56a31b0a13b3fa2
filipBirve <birve.f+github@gmail.com>
2023-07-22 11:06:26 +0200

    add r2 plot

--- simulate_data/Run_simplified_biclust_on_trivial_data.R
+++ simulate_data/Run_simplified_biclust_on_trivial_data.R
@@ -164,8 +164,29 @@ for(i_main in 1:max_iter){
     updated_cell_clust <- mapvalues(updated_cell_clust, from = unique_cell_cluster_ids, to = 1:n_cell_clusters)
     n_target_gene_clusters <- n_target_gene_clusters[unique_cell_cluster_ids]
   }
+
+
 }
 
+
+# Plot histograms of r2
+r2plot <- function(iteration, r2, prev_cell_clust){
+  if(T){
+    par(mfrow=c(length(unique(prev_cell_clust)),n_cell_clusters))
+    for(i_cells_from_cell_cluster in 1:length(unique(prev_cell_clust))){
+      for(i_fits_into_cell_cluster in 1:length(unique(prev_cell_clust))){
+        print(paste(i_cells_from_cell_cluster, i_fits_into_cell_cluster))
+        ind_for_cell_cluster = which(rep(1:n_cell_clusters, n_target_gene_clusters)==i_fits_into_cell_cluster)
+        hist(r2[ prev_cell_clust==i_cells_from_cell_cluster, ind_for_cell_cluster], breaks=100, main=paste("Cells from cell cluster", i_cells_from_cell_cluster, "\nfits into cell cluster", i_fits_into_cell_cluster, "with r2:"))
+      }
+    }
+    mtext(paste("Iteration", iteration), side = 3, line = -1, outer = TRUE)
+  }
+}
+
+r2plot(iteration = i_main,
+       r2 = r2,
+       prev_cell_clust = cell_cluster_history[,i_main + initial_column_padding])
 
 plot_cluster_history(cell_cluster_history = cell_cluster_history)
 
