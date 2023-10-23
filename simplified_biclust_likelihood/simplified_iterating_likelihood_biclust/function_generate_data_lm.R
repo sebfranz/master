e9ceed333e0fbd1b26f9d37e4225f13e8a15ec0a
sebfranz <seb.franzen@gmail.com>
2023-10-23 02:39:37 +0200

    Rename file and add sanity checks
    Rename file and add sanity checks
    for arguments.

--- simplified_biclust_likelihood/simplified_iterating_likelihood_biclust/function_generate_data_lm.R
+++ simplified_biclust_likelihood/simplified_iterating_likelihood_biclust/function_generate_data_lm.R
@@ -1,5 +1,61 @@
 library(tidyverse)
 
+#' Helper function for checking arguments
+#'
+#' Checks if an input argument is e.g. positive.
+#' @param x The input argument you want to check.
+#' @param one_element
+#' @param atomic
+#' @param numeric
+#' @param positive
+#' @param int
+#' @return void, or throws an error message
+#' @examples
+#'   checks(regulator_mean, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=FALSE)
+#' @noRd  # Keep this as an internal helper function
+checks <- function(x, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=TRUE){
+  error_message <- ""
+  if(one_element){
+    if (!(length(x) == 1L)) {
+      add_message <- paste0(deparse(substitute(x)), " must be one number.")
+      error_message <- paste(error_message, add_message, sep="\n")
+    }
+  }
+
+  if(atomic){
+    if(!is.atomic(x)) {
+      add_message <- (paste0(deparse(substitute(x)), " must be atomic."))
+      error_message <- paste(error_message, add_message, sep="\n")
+    }
+  }
+
+  if(numeric){
+    if (!is.numeric(x)) {
+      add_message <- (paste0(deparse(substitute(x)), " must be numeric."))
+      error_message <- paste(error_message, add_message, sep="\n")
+    }
+  }
+
+  if(positive){
+    if (!all(x > 0)) {
+      add_message <- (paste0(deparse(substitute(x)), " must be >0."))
+      error_message <- paste(error_message, add_message, sep="\n")
+    }
+  }
+
+  if(int){
+    if (!(all(round(x)==x))) {
+      add_message <- (paste0(deparse(substitute(x)), " must be an integer/s, now it's: ", toString(x)))
+      error_message <- paste(error_message, add_message, sep="\n")
+    }
+  }
+
+  if(nchar(error_message)>1){
+    stop(error_message)
+  }
+}
+
+
 #' Dummy data generation for biclust with lm
 #'
 #' Generates dummy data that work with biclust with lm
@@ -45,21 +101,50 @@ generate_data_lm <- function(n_cell_clusters = 3,
                              target_gene_type_standard_deviation = 3,
                              disturbed_fraction=0.1 # Value between 0 and 1. How large portion of cells should move to other cell clusters.
 ){
-  n_cell_clusters = 3
-  n_target_gene_type = 5  # We have x named target genes that have one expression per cell
-  n_regulator_gene_type = 20  # We have x named regulator genes that have one expression per cell
-  n_cells = c(1000,5000,10000)
-  regulator_means = c(1,2,3)  # For generating dummy data, regulator mean in each cell cluster
-  regulator_standard_deviations = c(0.1,0.2,0.3)  # For generating dummy data, regulator mean in each cell cluster
-  coefficients_standard_deviation = 100 # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
-  target_gene_type_standard_deviation = 3
-  disturbed_fraction=0.1
 
-  # Calculated variables
+
+  # Manual arguments ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  # n_cell_clusters = 3
+  # n_target_gene_type = 5  # We have x named target genes that have one expression per cell
+  # n_regulator_gene_type = 20  # We have x named regulator genes that have one expression per cell
+  # n_cells = c(1000,5000,10000)
+  # regulator_means = c(1,2,3)  # For generating dummy data, regulator mean in each cell cluster
+  # regulator_standard_deviations = c(0.1,0.2,0.3)  # For generating dummy data, regulator mean in each cell cluster
+  # coefficients_standard_deviation = 100 # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
+  # target_gene_type_standard_deviation = 3
+  # disturbed_fraction=0.1
+
+
+  # Check arguments -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  checks(n_cell_clusters, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=TRUE)
+  checks(n_target_gene_type, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=TRUE)
+  checks(n_regulator_gene_type, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=TRUE)
+  checks(n_cells, one_element=FALSE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=TRUE)
+  checks(regulator_means, one_element=FALSE, atomic=TRUE, numeric=TRUE, positive=FALSE, int=FALSE)
+  checks(regulator_standard_deviations, one_element=FALSE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=FALSE)
+  checks(coefficients_standard_deviation, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=FALSE)
+  checks(target_gene_type_standard_deviation, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=FALSE)
+  checks(disturbed_fraction, one_element=TRUE, atomic=TRUE, numeric=TRUE, positive=TRUE, int=FALSE)
+
+  if (length(n_cells) != n_cell_clusters) {
+    stop(paste0("n_cells has length ", length(n_cells), ", but must have length n_cell_clusters: ", n_cell_clusters, "."))
+  }
+
+  if (length(regulator_means) != n_cell_clusters) {
+    stop(paste0("regulator_means has length ", length(regulator_means), ", but must have length n_cell_clusters: ", n_cell_clusters, "."))
+  }
+
+  if (length(regulator_standard_deviations) != n_cell_clusters) {
+    stop(paste0("regulator_standard_deviations has length ", length(regulator_standard_deviations), ", but must have length n_cell_clusters: ", n_cell_clusters, "."))
+  }
+
+
+  # Calculated variables --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   n_total_cells <- sum(n_cells)
-  true_cell_cluster_allocation = rep(1:n_cell_clusters, times=n_cells)  # Cell cluster IDs will be in order
+  true_cell_cluster_allocation <- rep(1:n_cell_clusters, times=n_cells)  # Cell cluster IDs will be in order
 
-  # Generate regulator expressions for each cell cluster
+
+  # Generate regulator expressions for each cell cluster ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   regulator_gene_expression <- vector(mode='list', length = n_cell_clusters)
   for(i_cell_cluster in 1:n_cell_clusters){
     current_n_cells <- n_cells[i_cell_cluster]
@@ -72,7 +157,9 @@ generate_data_lm <- function(n_cell_clusters = 3,
   regulator_gene_expression <- do.call(rbind, regulator_gene_expression)
   colnames(regulator_gene_expression) <- paste0("r",1:n_regulator_gene_type)
   regulator_gene_expression <- tibble::as_tibble(regulator_gene_expression)
-  # Sample slopes
+
+
+  # Generate betas --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   # One per target gene type in each cell cluster meaning:
   # Two cell clusters with one target gene type in each: total number of betas=2
   # Two cell clusters with 3 respectively 4 target gene types: total number of betas=7
@@ -83,9 +170,14 @@ generate_data_lm <- function(n_cell_clusters = 3,
                                          sd =  coefficients_standard_deviation)
     # We want the different betas for each target gene type in the cell cluster, but the same across all regulator genes
     # Produces a matrix (Target gene types) x (Regulator gene types)
-    betas[[i_cell_cluster]] <- tibble::as_tibble(t(matrix(rep(temp_betas_for_cell_cluster, n_regulator_gene_type), ncol=n_regulator_gene_type)))
+    temp_betas <- t(matrix(rep(temp_betas_for_cell_cluster, n_regulator_gene_type), ncol=n_regulator_gene_type))
+    colnames(temp_betas) <- paste0("b",1:n_target_gene_type)
+    betas[[i_cell_cluster]] <- tibble::as_tibble(temp_betas)
   }
-  # Calculate target gene expressions for each cell cluster
+
+
+
+  # Calculate target gene expressions for each cell cluster ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   cell_cluster_target_gene_expression <- sapply(1:n_cell_clusters,
                                                 function(i_cell_cluster){
                                                   current_betas <- betas[[i_cell_cluster]]  # This picks out the relevant betas, as many as there are target gene types in this cell cluster
@@ -97,9 +189,14 @@ generate_data_lm <- function(n_cell_clusters = 3,
                                                   return( as.matrix(current_regulator_gene_expressions) %*% as.matrix(current_betas) + residuals)
                                                 }
   )
+  # Put it into a tibble
   target_gene_expression <- do.call(rbind, cell_cluster_target_gene_expression)
   colnames(target_gene_expression) <- paste0("t",1:n_target_gene_type)
   target_gene_expression <- tibble::as_tibble(target_gene_expression)
+
+
+
+  # Construct output data structure (also a tibble) -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dat <- tibble::tibble(cell_id = (1:n_total_cells),
                         true_cell_cluster_allocation,
                         target_gene_expression,
@@ -122,24 +219,25 @@ generate_data_lm <- function(n_cell_clusters = 3,
 # || interactive()
 if (sys.nframe() == 0){
   # ... do main stuff
-  library(ggplot2)  # To plot things #TODO: What things?
+  library(ggfortify)  # For pca-plot
 
-  # Simulate appropriate data from linear model
+  # Set seed for example
   set.seed(1234)
 
   dat <- generate_data_lm(n_cell_clusters = 3,
                           n_target_gene_type = 5,  # We have x named target genes that have one expression per cell
                           n_regulator_gene_type = 20,  # We have x named regulator genes that have one expression per cell
                           n_cells = c(1000,5000,10000),
-                          regulator_means = c(1,2,3),  # Regulator mean expression in each cell cluster.
+                          regulator_means = c(1,2,5),  # Regulator mean expression in each cell cluster.
                           regulator_standard_deviations = c(0.1,0.2,0.3),  # Regulator sd for expression in each cell cluster.
                           coefficients_standard_deviation = 100, # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
                           target_gene_type_standard_deviation = 3,
                           disturbed_fraction=0.1 # Value between 0 and 1. How large portion of cells should move to other cell clusters.
   )
 
-  dat[,'true_cell_cluster_allocation'] <- paste("Cluster", pull(dat, 'true_cell_cluster_allocation'))
+  dat[,'true_cell_cluster_allocation'] <- paste("Cluster", pull(dat, 'true_cell_cluster_allocation'))  # These needs to be strings for discrete labels in pca plot
   pca_res <- prcomp(dat[,3:ncol(dat)], scale. = TRUE)
-  ggplot2::autoplot(pca_res, data = dat, colour = 'true_cell_cluster_allocation')
-
+  p <- ggplot2::autoplot(pca_res, data = dat, colour = 'true_cell_cluster_allocation')
+  plot(p)
+  print(dat)
 }
